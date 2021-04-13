d3f32fa42f516cc632346d0907c3e5f3523d722a
sovereignLie@gmail.com <soverinLie@gmail.com>
2021-04-14 00:40:06 +0500

    init

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+import { BackgroundTriangle } from "../assets/icons/BackgroundTriangle";
+import Colors from "../constants/Colors";
+import useColorScheme from "../hooks/useColorScheme";
+
+export default function HomeScreen() {
+  const colorScheme = useColorScheme();
+  return (
+    <View style={styles.container}>
+      <View style={StyleSheet.absoluteFill}>
+        <BackgroundTriangle color={Colors[colorScheme].tabBarBackground} />
+      </View>
+    </View>
+  );
+}
